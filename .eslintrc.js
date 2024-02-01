9f205f392e1e03b7f54448bd7f9a3c18ce3e3b6c
Zazull <zazuleaweb@gmail.com>
2024-02-01 11:46:04 +0200

    initial commit

---
+++ .eslintrc.js
@@ -0,0 +1,28 @@
+module.exports = {
+    env: {
+        browser: true,
+        es2021: true,
+    },
+    extends: 'eslint:recommended',
+    overrides: [
+        {
+            env: {
+                node: true,
+            },
+            files: ['.eslintrc.{js,cjs}'],
+            parserOptions: {
+                sourceType: 'script',
+            },
+        },
+    ],
+    parserOptions: {
+        ecmaVersion: 'latest',
+    },
+    rules: {
+        indent: ['off'],
+        'linebreak-style': ['error', 'windows'],
+        quotes: ['warn', 'single'],
+        semi: ['error', 'always'],
+        'no-unused-vars': ['warn'],
+    },
+};
---
+++ js/script.js
@@ -0,0 +1,17 @@
+'use strict';
+
+// alert('privet mir, ti prekrasen');
+// const result = confirm('ti gei?');
+// console.log(result);
+
+// const answer = prompt('ti tocino ne gei?', 'samgei');
+// console.log(typeof answer);
+
+// const answers = [];
+// answers[] =
+
+let a = 10;
+
+console.log(a++);
+console.log(a++);
+console.log(a);
